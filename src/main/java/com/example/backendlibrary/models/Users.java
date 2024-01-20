afdfe25092b8895b977693cf702ecf51adc4e7cd
Johan Johnsson <johan@johnsson-net.se>
2024-01-20 14:36:20 +0100

    user model

--- src/main/java/com/example/backendlibrary/models/Users.java
+++ src/main/java/com/example/backendlibrary/models/Users.java
@@ -1,5 +1,16 @@
 package com.example.backendlibrary.models;
 
+import org.springframework.data.annotation.Id;
+
 public class Users {
 
+    @Id
+    private String id;
+    private String username;
+    private String name;
+    private String email;
+
+    public Users() {
+
+    }
 }
