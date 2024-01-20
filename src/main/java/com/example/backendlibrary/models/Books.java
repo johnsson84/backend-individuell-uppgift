5373260912d157e680e310cbd90ba099a0d9a98d
Johan Johnsson <johan@johnsson-net.se>
2024-01-20 14:34:05 +0100

    book model

--- src/main/java/com/example/backendlibrary/models/Books.java
+++ src/main/java/com/example/backendlibrary/models/Books.java
@@ -1,5 +1,18 @@
 package com.example.backendlibrary.models;
 
+import org.springframework.data.annotation.Id;
+
 public class Books {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String[] author_name;
+
+    public Books() {
+
+    }
+
 
 }
